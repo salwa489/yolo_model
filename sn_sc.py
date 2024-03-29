c7958d687777018a73b2f017c58c74c2dd0820b2
salwa489 <64315607+salwa489@users.noreply.github.com>
2024-03-29 03:04:38 +0200

    Update sn_sc.py

--- sn_sc.py
+++ sn_sc.py
@@ -7,18 +7,8 @@ import math
 
 st.title('Real-time Object Detection using YOLOv8')
 
-model = YOLO("../YOLO-Weights/yolov8n.pt")
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-]
+model = YOLO("model_- 25 march 2024 2_53.pt")
+classNames = ["Snak", "Scorpion"]
 
 # Function to perform object detection on the input image
 def detect_objects(img):
