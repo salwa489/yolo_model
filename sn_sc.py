f2654b6a872bd9a6343f81e8d99654dd78776bb2
salwa489 <64315607+salwa489@users.noreply.github.com>
2024-03-29 02:55:48 +0200

    Create sn_sc.py

---
+++ sn_sc.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+from ultralytics import YOLO
+import cv2
+import math
+
+st.title('Real-time Object Detection using YOLOv8')
+
+model = YOLO("../YOLO-Weights/yolov8n.pt")
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+]
+
+# Function to perform object detection on the input image
+def detect_objects(img):
+    img = np.array(img)
+    results = model(img, stream=True)
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            cls = int(box.cls[0])
+            class_name = classNames[cls]
+            label = f'{class_name} {conf}'
+            cv2.putText(img, label, (x1, y1-2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+
+    return img
+
+# Web Application
+st.text('Using the camera.')
+
+video = st.video('camera')
+
+while video:
+    frame = video
+    output_frame = detect_objects(frame)
+    st.image(output_frame, channels="BGR")
+
+st.stop()
